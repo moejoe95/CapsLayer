cc07c98ab8f74defc6c91ec26d8f448c47bc4b00
moejoe95 <[omitted]>
2020-11-01 14:33:11 +0100

    update plotting function

--- models/plot_csv.py
+++ models/plot_csv.py
@@ -1,40 +1,46 @@
 import sys
 import csv
 import numpy as np
+import pandas as pd
+import os.path
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 
+root = 'models/results/fashion_mnist_ResCaps'
 
-if len(sys.argv) <= 1:
-    print('need filename as argument')
+path_names = []
+i = 0
+while os.path.isdir(root + '_' + str(i)):
+    path_names.append(root + '_' + str(i))
+    i += 1
 
-filename = sys.argv[1]
+for path_name in path_names:
+    print('plotting: ', path_name)
+    try:
+        train = pd.read_csv(path_name + '/' + 'train_acc.csv')
+        test = pd.read_csv(path_name + '/' + 'val_acc.csv')
+    except:
+        print('failed to plot')
+        continue
 
-with open(filename) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    steps = []
-    values = []
-    value_id = None
-    for row in csv_reader:
-        if value_id is None:
-            value_id = row[1]
-            continue
-        steps.append(int(row[0]))
-        values.append(float(row[1]))
+    plt.scatter(train['step'], train['train_acc.csv'], label='train')
+    plt.scatter(test['step'], test['val_acc.csv'], label='validation')
 
-    plt.scatter(np.array(steps), np.array(values))
-
-    filename = filename.split('.')[0]
-    plt.title('Scatter plot of ' + filename)
+    plt.title('Scatter plot of ' + path_name)
     plt.xlabel('# training steps')
-    plt.ylabel(value_id)
+    plt.ylabel('accuracy')
+    plt.legend(loc="lower right")
 
     # set number of labels on x and y axis
     plt.locator_params(axis='x', nbins=5)
     plt.locator_params(axis='y', nbins=10)
 
     x1,x2,y1,y2 = plt.axis()
-    plt.axis((None,None,None,None))
+    plt.axis((None,None,0.0,1.0))
     
-    plt.savefig(filename + '.png')
+    filename = path_name.split('/')[-1]
+    plt.savefig('models/results/plots/' + filename + ".png")
+    plt.close()
+
+print('finished ...')
